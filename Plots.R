53eea3038c219eb4cb3067e9f93030869e5abb3f
Alex Howard <127774411+amh312@users.noreply.github.com>
2025-03-19 11:10:06 +0000

    standardised labelling

--- Plots.R
+++ Plots.R
@@ -1,7 +1,8 @@
-######PLOTS
+#PLOTS
 
-#Precision boxplot (observed prevalence)
+##Precision boxplot (observed prevalence)
 
+###Bind results into summary dataframe
 valid_norms <- data.frame(rbind(PEN_validation_norms,AMP_validation_norms,
                                 OXA_validation_norms,SAM_validation_norms,
                                 TZP_validation_norms,CZO_validation_norms,
@@ -15,41 +16,58 @@ valid_norms <- data.frame(rbind(PEN_validation_norms,AMP_validation_norms,
                                 TOB_validation_norms,NIT_validation_norms,
                                 SXT_validation_norms))
 
+###Convert to percentages
 valid_norms <- valid_norms %>% mutate(ratediff=ratediff*100)
 
+###Factorise antimicrobials
 levels_order <- valid_norms %>% group_by(Antimicrobial) %>% 
   mutate(iqr = IQR(ratediff)) %>% arrange(desc(iqr)) %>% 
   distinct(Antimicrobial) %>% unlist()
-
 valid_norms$Antimicrobial <- factor(valid_norms$Antimicrobial, levels = levels_order) %>% 
   fct_rev()
 
+###Set colour palette vector
 qual_col_pals <-  brewer.pal.info[brewer.pal.info$category == 'qual',]
-
 col_vector <-  unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
 
+###Boxplot
 valid_norms_plot <- ggplot(valid_norms, aes(x=Antimicrobial,y=ratediff,fill=Antimicrobial))+
+  
+  #main box
   geom_boxplot(outlier.shape = NA) +
+  
+  #outlier points
   geom_point(data = valid_norms %>% dplyr::filter(outlier),alpha=0.25) +
+  
+  #xy flip
   coord_flip() +
+  
+  #titles
   ggtitle(glue("AMR prevalence differences between observed prevalence estimates and actual data"))+
   xlab("Antimicrobial agent") +
   ylab("Prevalence estimation error (%)") +
+  
+  #plot width
   ylim(-30,30) +
+  
+  #set colour palette with vector (see above)
   scale_fill_manual(values=col_vector) +
+  
+  #themes
   theme(legend.position = "none") +
+  
+  #central line of no difference
   geom_hline(yintercept=0,linetype=2) +
+  
+  #extreme prevalence estimation error lines
   geom_hline(yintercept=-10,linetype=3,colour="red") +
   geom_hline(yintercept=10,linetype=3,colour="red")
 
 print(valid_norms_plot)
 
+##Precision boxplot (BEAR)
 
-
-
-
-#Precision validation boxplot (BEAR)
-
+###Compose dataframe
 valid <- data.frame(rbind(PEN_validation,AMP_validation,OXA_validation,
                           SAM_validation,TZP_validation,CZO_validation,
                           CXM_validation,CRO_validation,CAZ_validation,
@@ -58,46 +76,66 @@ valid <- data.frame(rbind(PEN_validation,AMP_validation,OXA_validation,
                           TCY_validation,VAN_validation,RIF_validation,
                           GEN_validation,AMK_validation,TOB_validation,
                           NIT_validation,SXT_validation))
+
+###Convert to percentages
 valid <- valid %>% mutate(ratediff=ratediff*100)
+
+###Set antimicrobial levels
 valid$Antimicrobial <- factor(valid$Antimicrobial, levels = levels_order) %>% 
   fct_rev()
+
+###Vector of colour palette
 qual_col_pals <-  brewer.pal.info[brewer.pal.info$category == 'qual',]
 col_vector <-  unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+###Boxplot
 valid_plot <- ggplot(valid, aes(x=Antimicrobial,y=ratediff,fill=Antimicrobial))+
+  
+  #box
   geom_boxplot(outlier.shape = NA) +
+  
+  #outlier points
   geom_point(data = valid %>% dplyr::filter(outlier),alpha=0.25) +
+  
+  #xy flip
   coord_flip() +
+  
+  #titles
   ggtitle(glue("AMR prevalence differences between BEAR estimates and actual data"))+
   xlab("Antimicrobial agent") +
   ylab("Prevalence estimation error (%)") +
+  
+  #plot width
   ylim(-30,30) +
+  
+  #set colours using palette vector (see above)
   scale_fill_manual(values=col_vector) +
   theme(legend.position = "none") +
+  
+  #central and extreme estimation error lines
   geom_hline(yintercept=0,linetype=2) +
   geom_hline(yintercept=-10,linetype=3,colour="red") +
   geom_hline(yintercept=10,linetype=3,colour="red")
 print(valid_plot)
 
+##Resource efficiency Cleveland dotplot
 
-
-
-
-#Resource dotplot
-
+###Set BEAR and OP labels on data and label plot df
 numbsims <- data.frame(cbind(numbsims,rep("BEAR",nrow(numbnorms))))
 numbnorms <- data.frame(cbind(numbnorms,rep("EOP",nrow(numbnorms))))
 numbsims[,2] <- as.numeric(numbsims[,2])
 numbnorms[,2] <- as.numeric(numbnorms[,2])
-
 colnames(numbsims) <- c("Antimicrobial","n","Estimator")
 colnames(numbnorms) <- c("Antimicrobial","n","Estimator")
 numbs <- data.frame(rbind(numbsims,numbnorms))
 
+###Factorise antimicrobials
 numbs$Antimicrobial <- factor(numbs$Antimicrobial, levels = numbs %>% 
                                 arrange(desc(n)) %>%
                                 distinct(Antimicrobial) %>% unlist()) %>% 
   fct_rev()
 
+###Calculate differences between BEAR and OP
 numbdiffs <- data.frame(numbsims[,2]-numbnorms[,2])
 colnames(numbdiffs) <- c("n2")
 numbdiffs$Antimicrobial <- numbsims$ab_name
@@ -105,7 +143,6 @@ numbdiffs <- numbdiffs %>% mutate(model = case_when(
   n2>0 ~ "BEAR",
   n2<0 ~ "EOP"
 ))
-
 numbdiffs$n2 <- abs(numbdiffs$n2)
 numbdiffs$Antimicrobial <- numbsims$Antimicrobial
 numbdiffs$BEAR <- numbsims$n
@@ -115,15 +152,30 @@ numbdiffs <- numbdiffs %>% mutate(n = case_when(
   model=="EOP"~EOP+5
 ))
 
+###Cleveland dotplot
 resource_plot <- ggplot(numbs, aes(x=Antimicrobial,y=n)) +
+  
+  #line denoting gap between approaches
   geom_line(aes(group=Antimicrobial),alpha=0.5)+
+  
+  #points denoting performance of bear and op approaches
   geom_point(aes(color=Estimator),size=4) +
+  
+  #xy flip
   coord_flip() +
+  
+  #titles
   ggtitle(glue("Minimum number of observed antimicrobial susceptibility results per pathogen required to avoid extreme estimation errors"))+
   xlab("Antimicrobial agent") +
   ylab("Number of observed results required") +
+  
+  #plot width
   ylim(0,110) +
+  
+  #blue for bear, green for op
   scale_color_manual(values=c("blue","green3"))+
+  
+  #annotate with performance differences
   geom_text(data = numbdiffs, aes(color = model, 
                                   label = as.character(glue("+{n2}"))),
             size = 3,hjust=0.5)
